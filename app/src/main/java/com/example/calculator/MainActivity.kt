3bd53dcabd1463683f2cda2b378a2e3a442a5235
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-05-03 09:42:16 +0900

    db사용하기 위해 build후 할당, 연산식과 결과를 보여줄 textView lazy사용

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.room.Room
+import com.example.calculator.database.CalDatabase
 
 class MainActivity : AppCompatActivity() {
 
+    // 계산 결과 textView lazy
+    private val tvResult: TextView by lazy {
+        findViewById(R.id.tvResult)
+    }
+
+    // 연산식 textView lazy
+    private val tvCalculation: TextView by lazy {
+        findViewById(R.id.tvCalculation)
+    }
+    
+    private var hasOperator = false // 지금은 연산자를 한번만 사용하도록
+
+    lateinit var db: CalDatabase // 전역
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // create시 db 변수에 데이터베이스 빌드해서 할당
+        db = Room.databaseBuilder(
+            applicationContext,
+            CalDatabase::class.java,
+            "HistoryDB"
+        ).build()
     }
 }
